5aab1c6c16554a7ab8115358749809267b5cea07
Arthur Henrique <87656157+black258br@users.noreply.github.com>
2023-01-05 08:38:12 -0300

    Projeto Todo

---
+++ todo/src/App.js
@@ -0,0 +1,81 @@
+import './App.css';
+import axios from "axios";
+import api from "../api/todos";
+import React, { useState, useEffect } from 'react';
+import { BsTrash, BsBookmarkCheck, BsBookmarkCheckFill} from "react-icons/bs";
+
+const API = "https://localhost:5000";
+
+function App() {
+  const [title, setTitle] = useState("");
+  const [time, setTime] = useState ("");
+  const [todos, setTodos] = useState ([]);
+  const [loading, setLoading] = useState (false);  
+
+  const retrieveTodos = async () => {
+    const response = api.get("/todos");
+    return response.data;
+  }
+
+  useEffect(() => {
+    const getAllTodos = async () => {
+      const allTodos = await retrieveTodos();
+      if (allTodos) setTodos(allTodos);
+    };
+
+    getAllTodos();
+  })
+
+  const handleSubmit = (e) => {
+    e.preventDefault(); //e previne que a pagina seja recarregada ao enviar 
+    const request = {
+      id:uuid
+    }
+
+
+    setTitle("");
+    setTime("");
+  }
+
+  return (
+    <div className="App">
+      <div className="todo-header">
+        <h1>React Todo</h1>
+      </div>
+      <div className="form-todo">
+        <h2>Insira a sua proxima tarefa:</h2>
+        <form onSubmit={handleSubmit}>
+          <div className="form-control">
+            <label htmlFor="title">Tarefa</label>
+            <input
+              type="text"
+              name="title"
+              placeholder="Titulo da tarefa"
+              onChange={(e) => setTitle(e.target.value)}
+              value={title || ""}
+              required
+            />
+          </div>
+          <div className="form-control">
+            <label htmlFor="time">Duração</label>
+            <input
+              type="text"
+              name="time"
+              placeholder="Tempo em horas"
+              onChange={(e) => setTime(e.target.value)}
+              value={time || ""}
+              required
+            />
+          </div>
+          <input type="submit" value="Criar Tarefa"/>
+        </form>
+      </div>
+      <div className="list-todo">
+        <p>Lista de tarefas:</p>
+        {todos.length === 0 && <p>Não há tarefas!</p>}
+      </div>
+    </div>
+  );
+}
+
+export default App;
